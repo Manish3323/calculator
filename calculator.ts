8e4bd01ec28878a2b3ea080401a64edbd01f8c43
Manish <gowardipemanish95@gmail.com>
2024-08-08 23:46:08 +0530

    Update variable names

--- calculator.ts
+++ calculator.ts
@@ -1,23 +1,30 @@
 export const calculateSum = (numbers: string) => {
   const containsDelimiter = numbers.startsWith("//");
   //assuming pattern //[delimiter] hence magic number 3 & 5
-  const delimiterIndex = 3
-  const inputStartIndex = 5
+  const delimiterIndex = 3;
+  const inputStartIndex = 5;
   const delimiter = containsDelimiter ? numbers.at(delimiterIndex) || "," : ",";
-  const input = containsDelimiter ? numbers.substring(inputStartIndex) : numbers;
+  const input = containsDelimiter
+    ? numbers.substring(inputStartIndex)
+    : numbers;
 
-  const either: { left: number[]; right: number[] } = { left: [], right: [] };
+  const either: { negatives: number[]; normals: number[] } = {
+    negatives: [],
+    normals: [],
+  };
   input
     .replace("\n", delimiter)
     .split(delimiter)
     .reduce((e, x) => {
       const integer = Number(x);
-      integer < 0 ? e.left.push(integer) : e.right.push(integer);
+      integer < 0 ? e.negatives.push(integer) : e.normals.push(integer);
       return e;
     }, either);
 
-  if (either.left.length > 0)
-    throw new Error(`negative numbers not allowed ${either.left.join(",")}`);
+  if (either.negatives.length > 0)
+    throw new Error(
+      `negative numbers not allowed ${either.negatives.join(",")}`
+    );
 
-  return either.right.reduce((x: number, sum: number) => x + sum, 0);
+  return either.normals.reduce((x: number, sum: number) => x + sum, 0);
 };
